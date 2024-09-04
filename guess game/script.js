79b1334104f3c37183ef9a853ab044ae772d47a2
Mohamedibrahim01 <112860022+Mohamedibrahim01@users.noreply.github.com>
2024-09-04 14:54:05 +0200

    Add files via upload

--- guess game/script.js
+++ guess game/script.js
@@ -5,8 +5,8 @@ let highscore = 0;
 console.log(secretNumber);
 
 const displayMessage = function (message) {
-   document.querySelector('.message').textContent = message;
-  };
+  document.querySelector(".message").textContent = message;
+};
 
 //document.querySelector(".number").textContent = secretNumber;
 
@@ -14,25 +14,35 @@ document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
   console.log(guess, typeof guess);
 
-
-  if(!guess){
-    displayMessage('No Secret Number!')
-  }else if(guess === secretNumber){
-    displayMessage('Correct')
-  }else if (guess > secretNumber && score > 0){
-    displayMessage('Too high')
-    document.querySelector('.score').textContent = score;
+  if (!guess) {
+    displayMessage("No Secret Number!");
+  } else if (guess === secretNumber) {
+    displayMessage("Correct");
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector(".highscore").textContent = highscore;
+    }
+  } else if (guess > secretNumber && score > 0) {
+    displayMessage("Too high");
+    document.querySelector(".score").textContent = score;
     score--;
-  }else if(guess < secretNumber && score > 0){
-    displayMessage('Too low')
-    document.querySelector('.score').textContent = score;
+  } else if (guess < secretNumber && score > 0) {
+    displayMessage("Too low");
+    document.querySelector(".score").textContent = score;
     score--;
-  }else if (score == 0){
-    displayMessage('You Lost')
-    document.querySelector('.score').textContent = 0;
+  } else if (score == 0) {
+    displayMessage("You Lost");
+    document.querySelector(".score").textContent = 0;
   }
 });
 // Reload the page when the "Again!" button is clicked
-document.querySelector(".again").addEventListener("click", function() {
-  location.reload();
+document.querySelector(".again").addEventListener("click", function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  displayMessage("Start guessing...");
+  document.querySelector(".score").textContent = score;
+  document.querySelector(".number").textContent = "?";
+  document.querySelector(".guess").value = "";
+
+  
 });
